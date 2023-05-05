5b7ad0b74b8199356c52c86acfc61fc5ec594825
luishqsantos <luis_hsjunior@outlook.com>
2023-05-05 09:57:27 -0300

    feat:Add current page on navbar and sidebar

--- resources/views/site/layouts/_partials/header.blade.php
+++ resources/views/site/layouts/_partials/header.blade.php
@@ -8,12 +8,23 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-link text-white" href="{{ route('site.index') }}">Home</a>
-                <a class="nav-link text-white" href="{{ route('site.about') }}">Sobre</a>
-                <a class="nav-link text-white" href="{{ route('site.contact') }}">Contato</a>
-                <a class="nav-link text-white" href="{{ route('site.login') }}">Login</a>
-            </div>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->routeIs('site.index') ? 'active' : '' }}" href="{{ route('site.index') }}">Home</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->routeIs('site.about') ? 'active' : '' }}" href="{{ route('site.about') }}">Sobre</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->routeIs('site.contact') ? 'active' : '' }}" href="{{ route('site.contact') }}">Contato</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->routeIs('site.login') ? 'active' : '' }}" href="{{ route('site.login') }}">Login</a>
+                </li>
+            </ul>
         </div>
     </div>
 </nav>
