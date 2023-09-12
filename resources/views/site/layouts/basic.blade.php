c302c52b7e34662a5aa6b2892eeb0a1a01618698
luishqsantos <luis_hsjunior@outlook.com>
2023-09-12 15:01:49 -0300

    refactor: adjust the active link in the user module

--- resources/views/site/layouts/basic.blade.php
+++ resources/views/site/layouts/basic.blade.php
@@ -8,6 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="shortcut icon" href="{{ asset('img/logo.png') }}" type="image/x-icon">
 
     <title>Super Gestão - @yield('title')</title>
 </head>
